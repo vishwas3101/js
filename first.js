c835bb7797364ae4299723b886f8182835226344
vishwas3101 <vishwasjain31@gmail.com>
2024-06-13 17:52:38 +0530

    new files added

---
+++ first.js
@@ -0,0 +1,86 @@
+// const identity={
+//     age:20,
+//     name:"vishwas",
+//     cgpa:4.67,
+//     doing:"web_dev",
+//     result:true,
+
+
+// };
+// console.log (identity)
+// typeof identity
+
+// const item={
+//     itemname:'parker',
+//     rating:3.5,
+//     price:400,
+//     discount:5,
+//     color:'blue',
+//     users:5666,
+//     deal_of_month:true
+
+// };
+// console.log(item)
+// const a=99;
+// let result;
+// if (a%2==0){
+//     result="even";
+// }
+// else {
+//     result="odd";
+// }
+// console.log(a," is ",result)
+
+// let gamenumber=25;
+// let usernumber=prompt("enter your number");
+// if (gamenumber==usernumber){
+//     console.log("u r winn");
+// }
+// else{
+//     console.log("try again");
+
+
+// }
+//calci
+// let g=prompt("enter the 1 value ");
+// let v=prompt("enter the second value");
+// let result;
+// let u=prompt("choose the function 1 for add,2 for sub,3 for mul,4 for div");
+// if (u==1){
+//     result=g+v;
+//     console.log(result);
+
+// }
+// else if (u==2){
+//     result=g-v;
+//     console.log(result);
+
+// }
+// else if (u==3){
+//     result=g*v;
+//     console.log(result);
+
+// }
+// else if (u==4){
+//     result=g/v;
+//     console.log(result);
+
+// }
+// else {console.log("invalid choice")}
+
+// let vishwas={
+//     phone_brand:"iphone",
+//     shorts:"forclaz",
+//     shoesno:9,
+//     mmobile_phone_number:8905477499
+// };
+
+// let cow=`love the way u wanna with a ${vishwas.phone_brand} is very uch `;
+// console.log(cow);
+// let dog="minchi";
+// console.log(dog.length);
+// console.log(typeof dog);
+
+let str =prompt("u r name w/o space");
+let answer="@"+str+str.length;
+console.log(answer);
